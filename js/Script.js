3162222aef16aa20880c7ce4e02f9a799d520c80
kemal <yavuz.imer133@gmail.com>
2021-07-10 20:14:11 +0300

    e-commerce

---
+++ js/Script.js
@@ -0,0 +1,15 @@
+/* Ufak cihazlar için menü açma butonunun işlemleri */ 
+
+// header-menu-open-button-field
+function menuopen(){
+    let x = document.getElementById("header-menu-button-limit-field");
+    if(x.style.display=="block"){
+        x.style.display="none";
+    }
+    else{
+        x.style.display="block";
+    }
+}
+
+/* Ufak cihazlar için menü açma butonunun işlemleri */ 
+
